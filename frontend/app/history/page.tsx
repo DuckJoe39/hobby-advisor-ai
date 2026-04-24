b395dd1e37798f89de9b4e21c259f4662abfccbd
DuckJoe39 <tanimuradaijiro@gmail.com>
2026-04-24 17:06:30 -0400

    フロントエンド: APIのURLを環境変数に変更

--- frontend/app/history/page.tsx
+++ frontend/app/history/page.tsx
@@ -16,7 +16,7 @@ export default function HistoryPage() {
   useEffect(() => {
     const fetchHistory = async () => {
       try {
-        const res = await fetch("http://127.0.0.1:8000/api/history");
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/questions`);
         const data = await res.json();
         if (data.status === "success") {
           setHistories(data.data);
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const res = await fetch("http://127.0.0.1:8000/api/questions");
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/questions`);
         const data = await res.json();
         if (data.status === "success") {
           setQuestions(data.data);
